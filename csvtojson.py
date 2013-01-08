8fe5335995d676496d36b77cdcdf2e76b2a09359
James Tautges <jamesdtautges@gmail.com>
2013-01-07 22:04:20 -0600

    Reworked csvtojson.py to accommodate the current output of AMC and added more comments than I'm comfortable with.

--- csvtojson.py
+++ csvtojson.py
@@ -1,22 +1,30 @@
 import json, sys
 
-f = open(sys.argv[1], 'r')
+f = open(sys.argv[1], 'r') # open file specified by the first argument to the function
 
-lines = f.readlines()
+lines = f.readlines() # read raw data into list
 csv = []
 for line in lines:
-    csv.append(line.replace('"', '').replace('\n', '').split(';'))
+    csv.append(line.replace('"', '').replace('\n', '').split(';')) # read lines of CSV into 2 x (number of tags) list ignoring quotes and newlines and spliting at semicolons
 
-dic = {'tags': csv[0]}
-csv.pop(0)
 l = []
+for tag in csv[0]: # take only the tags that contain more than a single digit
+    if '.' not in tag:
+        l.append(tag)
+dic = {'tags': l} # assign tags to the list of important tags
 d = {}
-for x in range(len(csv)):
-    for i in range(len(dic['tags'])):
-        d[dic['tags'][i]]=csv[x][i]
-    l.append(d)
-dic['data'] = l
 
-j = json.dumps(dic)
+i=0
+while i < len(csv[0]): # loop through the tags and assign tags to their values
+    while '.' in csv[0][i]: # just keep going if the tag contains a '.'
+        i += 1
+    if i >= len(csv[0]): # make sure we haven't overshot the lenth of the list before trying to access it
+        break
+    d[csv[0][i]] = csv[1][i] # set key for tag to its value
+    i += 1
+dic['data'] = d # add dictionary to data tag in the main one
+
+j = json.dumps(dic) # convert dictionary to JSON
 
 print j
+

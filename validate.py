a79186b9fca1dcc4fa64bf176d8f6853b59fa445
newtonwolfe <newton.wolfe@team1306.com>
2013-02-02 20:34:52 -0600

    Climbing points can now be zero without throwing an error.

--- validate.py
+++ validate.py
@@ -18,4 +18,4 @@ teampoints = [] #a big list with all teams and their average points
 
 for team in dictionary["data"]:
     for rnd in dictionary["data"][team]:
-        assert (int(rnd["climberpts"]) == 10 or int(rnd["climberpts"]) == 20 or int(rnd["climberpts"]) == 30),"Climber points are invalid! Team %d, round %d." % (int(team), int(rnd["roundnumber"]))
+        assert (int(rnd["climberpts"]) == 0 or int(rnd["climberpts"]) == 10 or int(rnd["climberpts"]) == 20 or int(rnd["climberpts"]) == 30),"Climber points are invalid! Team %d, round %d." % (int(team), int(rnd["roundnumber"]))

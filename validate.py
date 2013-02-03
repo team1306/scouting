a3dac265532e361b1ed9f5eef285e5b236b1e343
newtonwolfe <newton.wolfe@team1306.com>
2013-02-02 20:26:29 -0600

    Started validation code, currently checks climbing points. Added problem in exampleData to test functionality.

---
+++ validate.py
@@ -0,0 +1,21 @@
+import json
+import sys
+
+jsonFile = sys.argv[1]
+
+jsonFileLines = open(jsonFile)
+lines = jsonFileLines.readlines()
+jsonFileLines.close()
+
+s = ""
+for line in lines:
+    s += line.strip("\n")
+
+dictionary = (json.loads(s)) #this is the dictionary
+dictionarylength = (len(dictionary["data"]))
+temporary = [] #add up a teams points per round here temporarily
+teampoints = [] #a big list with all teams and their average points
+
+for team in dictionary["data"]:
+    for rnd in dictionary["data"][team]:
+        assert (int(rnd["climberpts"]) == 10 or int(rnd["climberpts"]) == 20 or int(rnd["climberpts"]) == 30),"Climber points are invalid! Team %d, round %d." % (int(team), int(rnd["roundnumber"]))

579a0052df6b52430d91544463ccbf6f1269801e
newtonwolfe <newton.wolfe@team1306.com>
2013-02-04 19:33:40 -0600

    Swapped assertion for an if statement to declutter output.

--- validate.py
+++ validate.py
@@ -18,4 +18,7 @@ teampoints = [] #a big list with all teams and their average points
 
 for team in dictionary["data"]:
     for rnd in dictionary["data"][team]:
-        assert (int(rnd["climberpts"]) == 0 or int(rnd["climberpts"]) == 10 or int(rnd["climberpts"]) == 20 or int(rnd["climberpts"]) == 30),"Climber points are invalid! Team %d, round %d." % (int(team), int(rnd["roundnumber"]))
+        climberpts = int(rnd["climberpts"])
+        # assert (climberpts == 0 or climberpts == 10 or climberpts == 20 or climberpts == 30),"Climber points are invalid! Team %d, round %d." % (int(team), int(rnd["roundnumber"]))
+        if (climberpts != 0 and climberpts != 10 and climberpts != 20 and climberpts != 30):
+            print "Climber points are invalid! Team %d, round %d." % (int(team), int(rnd["roundnumber"]))

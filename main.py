8240f09a5dbf0e511811bc12e075128737743137
James Tautges <jamesdtautges@gmail.com>
2013-02-17 13:28:44 -0600

    Changed default file location.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csvtojson, ranking, argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", default="~/MC-Projects/AMCscouting2013/exports/AMCscouting2013.csv")
+parser.add_argument("-f", "--file", default="./AMCscouting2013.csv")
 parser.add_argument("-s", "--shooter", type=float, help="shooter weighting factor", default=1)
 parser.add_argument("-c", "--climber", type=float, help="climber weighting factor", default=1)
 

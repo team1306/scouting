f641c518d2d24f8bb5f68931900ae9e65987b21b
James Tautges <jamesdtautges@gmail.com>
2013-02-17 13:01:00 -0600

    Realized there was actually no problem with ranking.py and turned it into an importable file.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import csvtojson, sys, argparse
+import csvtojson, ranking, argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", 
+parser.add_argument("-f", "--file", default="~/MC-Projets/AMCscouting2013/exports/AMCscouting2013.csv")
+parser.add_argument("-s", "--shooter", type=float, help="shooter weighting factor", default=1)
+parser.add_argument("-c", "--climber", type=float, help="climber weighting factor", default=1)
 
-if len(sys.argv) > 1:
-    f = sys.argv[1]
+args = parser.parse_args()
 
-else:
-    f = "~/MC-Projects/AMCscouting2013/exports/AMCscouting2013.csv"
-
-
-
+print rank(csvtojson(args.file), args.climber, args.shooter)
--- ranking.py
+++ ranking.py
@@ -2,54 +2,56 @@ import json
 import sys
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file")
-parser.add_argument("-s", "--shooter", type=float, help="shooter weighting factor", default=1)
-parser.add_argument("-c", "--climber", type=float, help="climber weighting factor", default=1)
-args = parser.parse_args()
 
-jsonFile = args.file
-climberWeight = args.climber
-shooterWeight = args.shooter
+def rank(dictionary, climberWeight, shooterWeight):
+    dictionarylength = (len(dictionary["data"]))
 
-jsonFileLines = open(jsonFile)
-lines = jsonFileLines.readlines()
-jsonFileLines.close()
+    x = 0
+    temporary = [] #add up a teams points per round here temporarily
+    teampoints = [] #a big list with all teams and their average points
 
-s = ""
-for line in lines:
-    s += line.strip("\n")
-
-dictionary = (json.loads(s)) #this is the dictionary
-dictionarylength = (len(dictionary["data"]))
-
-x = 0
-temporary = [] #add up a teams points per round here temporarily
-teampoints = [] #a big list with all teams and their average points
-
-for team in dictionary["data"]:
-    print team
-    for rnd in dictionary["data"][str(team)]:
-        print rnd
-        climberTotal = int(rnd["climberpts"])*climberWeight
+    for team in dictionary["data"]:
+        print team
+        for rnd in dictionary["data"][str(team)]:
+            print rnd
+            climberTotal = int(rnd["climberpts"])*climberWeight
         
-        
-        highPoints = int(rnd["telehighgoals"])*3
-	medPoints = int(rnd["telemedgoals"])*2
-	lowPoints = int(rnd["telelowgoals"])
+            
+            highPoints = int(rnd["telehighgoals"])*3
+            medPoints = int(rnd["telemedgoals"])*2
+            lowPoints = int(rnd["telelowgoals"])
+            
+            highPointsAuto = int(rnd["autohighgoals"])*6
+            medPointsAuto = int(rnd["automedgoals"])*5
+            lowPointsAuto = int(rnd["autolowgoals"])*4      
+            
+            shooterTotal = (highPoints+medPoints+lowPoints+highPointsAuto+medPointsAuto+lowPointsAuto)*shooterWeight
+            temporary.append(climberTotal+shooterTotal)
+        teampoints.append([int(team), sum(temporary)/len(temporary)])
+        temporary = []
 
-	highPointsAuto = int(rnd["autohighgoals"])*6
-	medPointsAuto = int(rnd["automedgoals"])*5
-	lowPointsAuto = int(rnd["autolowgoals"])*4      
-        
-        shooterTotal = (highPoints+medPoints+lowPoints+highPointsAuto+medPointsAuto+lowPointsAuto)*shooterWeight
-        temporary.append(climberTotal+shooterTotal)
-    teampoints.append([int(team), sum(temporary)/len(temporary)])
-    temporary = []
+        teampoints.sort(key=lambda x: x[1]) #this does the sorting
 
-teampoints.sort(key=lambda x: x[1]) #this does the sorting
+        return teampoints
 
-print teampoints
-
-                       
-#print(d["data"]["1306"][0]["climberpts"]) #getting specific data
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file")
+    parser.add_argument("-s", "--shooter", type=float, help="shooter weighting factor", default=1)
+    parser.add_argument("-c", "--climber", type=float, help="climber weighting factor", default=1)
+    args = parser.parse_args()
+    
+    jsonFile = args.file
+    climberWeight = args.climber
+    shooterWeight = args.shooter
+    
+    jsonFileLines = open(jsonFile)
+    lines = jsonFileLines.readlines()
+    jsonFileLines.close()
+    
+    s = ""
+    for line in lines:
+        s += line.strip("\n")
+    
+    dictionary = (json.loads(s)) #this is the dictionary
+    print rank(dictionary, climberWeight, shooterWeight)

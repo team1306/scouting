ff86aed53a6b260b576a715e2bbbdd9eb74cccfc
James Tautges <jamesdtautges@gmail.com>
2013-02-26 20:39:34 -0600

    Commented main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csvtojson, ranking, argparse
 
+# parse the tags to get the name of the input file and the respective climbing and shooting weights
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", default="./AMCscouting2013.csv")
 parser.add_argument("-s", "--shooter", type=float, help="shooter weighting factor", default=1)
@@ -7,4 +8,5 @@ parser.add_argument("-c", "--climber", type=float, help="climber weighting facto
 
 args = parser.parse_args()
 
+# print the resulting ranking
 print ranking.rank(csvtojson.csvtojson(args.file), args.climber, args.shooter)
